156078bbd97ae9679c873fba7c2b4315d63192b5
kilik42 <marvinevins@yahoo.com>
2021-12-11 20:54:21 -0600

    working on collection card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className ="app">
          <Header/>
+         <CollectionCard id ={0} name={'cyber celesital'} traits={[{'value': 7}]}
+         image='https://lh3.googleusercontent.com/khhnfp0Z_o83TEUzZ1Inh6PfQgFca2Skbx2bLCaW5IM-xqC8EPFg5aDUEqJCjLwEujz-48EYJ-AcrhF8FNiM4It31_fqh_mwMxRKCEc=w600'/>
     </div>
    
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Header.css';
 import punkLogo from '../assets/cryptopunk-logo.png';
 import searchIcon from './assets/search.png';
-import themeSwitchIcon from './assets/theme-switch.png'
+import themeSwitchIcon from '../assets/theme-switch.png'
 const Header = () => {
     return (
         <div className='header'>
